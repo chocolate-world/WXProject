e27a91d44b0f7fd01ec0f79f67792e233bbd11b5
chocolate-world <15822932963@163.com>
2019-02-25 22:06:35 +0800

    first commit

---
+++ src/test/java/com/imooc/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.imooc.service.impl;
+
+import com.imooc.dataobject.ProductInfo;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductServiceImplTest {
+
+    @Autowired
+    private ProductServiceImpl productService;
+
+    @Test
+    public void findOne() throws Exception {
+
+        ProductInfo productInfo = productService.findOne("123456");
+        Assert.assertEquals("123456", productInfo.getProductId());
+    }
+
+    @Test
+    public void findUpAll() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void save() {
+    }
+}
